e2163c16f954313a0a3dd81e864e319650782be6
Nick Jackson <nickdjackson@msn.com>
2023-04-07 16:33:06 -0400

    changes to form

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function register(Request $request) {
         $incomingFields = $request->validate([
-            'username' => ['required', 'min:3', 'max: 20', Rule::unique('users', 'username')],
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
             'password'=> ['required', 'min:8', 'confirmed']
         ]);
